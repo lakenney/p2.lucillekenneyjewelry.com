ead3825b13a46654c89eec98792df14b2e241118
Lucille Kenney <lucilleakenney@gmail.com>
2013-10-15 13:38:35 -0400

    Added users signup
    Completed Framework up to Sign up.

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -17,7 +17,7 @@ class practice_controller extends base_controller {
 		$imageObj = new Image('http://placekitten.com/1000/1000');
 	
 		// Then we have access to all the methods within that object
-		// and we can use the methods in that class
+		// and we can point to the methods in that class
 		$imageObj->resize(500,500);
 	
 		$imageObj->display();
@@ -30,7 +30,37 @@ class practice_controller extends base_controller {
 		echo Time::now();
 	
 	}
+	
+	public function test3() {
+	
+	# Our SQL command
+	$q = "INSERT INTO users SET 
+    	first_name = 'Sam', 
+    	last_name = 'Seaborn',
+    	email = 'seaborn@whitehouse.gov'";
 
+	# Run the command
+	echo DB::instance(DB_NAME)->query($q);
+	}
+
+	// Our Update SQL command
+	public function test4() {
+	$q = "UPDATE users
+    	SET email = 'samseaborn@whitehouse.gov'
+    	WHERE email = 'seaborn@whitehouse.gov'";
+
+	# Run the command
+	echo DB::instance(DB_NAME)->query($q);
+	}
+	
+	// Our Delete SQL command
+	public function test5() {
+	$q = "DELETE FROM users
+    	WHERE email = 'sam@whitehouse.gov'";
+
+	# Run the command
+	echo DB::instance(DB_NAME)->query($q);
+	}
 
 }  # eoc
 
