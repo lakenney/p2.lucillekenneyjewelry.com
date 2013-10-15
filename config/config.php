626f42a98c0c25eb4bf540adb36aa762922b3719
Lucille Kenney <lucilleakenney@gmail.com>
2013-10-15 19:07:22 -0400

    config edits
    Renamed database with prefix lucillek_

--- config/config.php
+++ config/config.php
@@ -28,10 +28,10 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'webmaster@lucillekenneyjewelry.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "p2_lucillekenneyjewelry_com"); 
+	define('DB_NAME', "lucillek_p2_lucillekenneyjewelry_com"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
