f7afadbffbb39c5275c8762b1f8e61da8dc1cecf
Lucille Kenney <lucilleakenney@gmail.com>
2013-10-23 16:22:10 -0400

    Posts, Adds, Follow added pre class 8
    need to update live database

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,9 +1,27 @@
 <form method='POST' action='/posts/p_add'>
 
-    <br>
-    <textarea  cols="60" rows="10" name='comment'>Say something nice.</textarea>
+    <label for='content'>New Post</label><br>
+    <textarea  cols="60" rows="10" name='content' id='content'>Say something nice.</textarea>
+    
     <br><br>
-
-    <input type='submit' value='Add Post'>
+    <input type='submit' value='New Post'>
 
 </form>
+
+<br> <br>
+<!--<article>
+<?php foreach($posts as $post): ?>
+
+
+
+    <h1><?=$post['first_name']?> <?=$post['last_name']?> posted:</h1>
+
+    <p><?=$post['content']?></p>
+
+    <time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+        <?=Time::display($post['created'])?>
+    </time>
+
+</article>
+
+<?php endforeach; ?>-->
